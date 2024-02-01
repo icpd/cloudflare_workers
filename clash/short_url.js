4adddc7399e24f7bccddddb64221018fa176876c
icpd <35096485+icpd@users.noreply.github.com>
2024-02-01 19:38:52 +0800

    Create short_url.js

---
+++ clash/short_url.js
@@ -0,0 +1,20 @@
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+    const {method, headers} = request;
+    const url = new URL(request.url);
+    const path = url.pathname.slice(1); // Remove the leading '/'
+
+    if (!path) {
+        return new Response('Forbidden', {status: 403});
+    }
+  
+    const longUrl = await URLS.get(path);
+    if (longUrl) {
+        return Response.redirect(longUrl, 302);
+    }
+
+    return new Response('URL not found', {status: 404});
+}
